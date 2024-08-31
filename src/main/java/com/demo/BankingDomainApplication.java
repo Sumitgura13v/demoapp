914cc0c6e0855ac9285e4b08a20db59ca03d51f4
Sumitgura13v <88160099+Sumitgura13v@users.noreply.github.com>
2024-08-31 23:19:25 +0530

    Update BankingDomainApplication.java
    added sumit in Sout

--- src/main/java/com/demo/BankingDomainApplication.java
+++ src/main/java/com/demo/BankingDomainApplication.java
@@ -8,7 +8,7 @@ public class BankingDomainApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingDomainApplication.class, args);
-		System.out.println("Hello World");
+		System.out.println("Hello World Sumit");
 	}
 
 }
